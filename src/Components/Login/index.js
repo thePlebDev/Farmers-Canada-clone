32ef4fa5f00eb8a5861a7e247b9a990bc01a0521
Tristan Elliott <>
2021-01-30 17:10:11 -0400

    adding login form

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -7,24 +7,74 @@ import LoginTextInput from '../LoginTextInput'
 
 
 const Container = styled.div`
-  border:1px solid blue;
+
   width:100%;
-  margin:0 auto;
+  margin:20px auto;
 `
 
 const FormContainer = styled.div`
-  max-width:400px;
+  max-width:240px;
   border:1px solid #29b706;
   margin:10px auto;
   padding:30px;
   border-radius:4px;
+  @media only screen and (min-width: 770px) {
+    max-width:330px
+}
 `
 
 const Text = styled.h3`
   font-weight:500;
   color:#29b706;
   font-size:27px;
+`
 
+const Button = styled.button`
+ background-color:#29b706;
+ border-radius:4px;
+ text-align:center;
+ padding:10px 20px;
+ border:none;
+ width:100px;
+ color:white;
+ font-size:15px;
+ font-weight:500;
+`
+
+const Lost = styled(Text)`
+  font-weight:300;
+  font-size:17px;
+`
+const Farmers = styled.div`
+  color:#767676;
+  font-weight:400;
+  font-size:14px;
+  text-align:center;
+  margin:20px auto;
+  display:flex;
+  flex-direction:row;
+   &:after{
+     content:'';
+     flex:1 1;
+     border-bottom:1px solid #767676;
+     margin:auto;
+   };
+   &:before{
+     content:'';
+     flex:1 1;
+     border-bottom:1px solid #767676;
+     margin:auto;
+   }
+`
+
+const BecomeVendor = styled.div`
+  text-align:center;
+  padding:9px 10px;
+  border-radius:4px;
+  margin-top:25px;
+  font-weight:500;
+  color:#29b706;
+  border:1px solid #29b706;
 `
 
 
@@ -38,11 +88,14 @@ const Login =()=>{
         <Text>Sign In</Text>
 
         <SearchForm>
-          <LoginTextInput/>
-          <LoginTextInput/>
+          <LoginTextInput label={'Username or email address'}/>
+          <LoginTextInput label={'Password'}/>
 
+          <Button type="submit" >Sign In</Button>
         </SearchForm>
-
+          <Lost>Lost your password ?</Lost>
+          <Farmers>New to Farmers Canada ?</Farmers>
+          <BecomeVendor>Create an Account</BecomeVendor>
       </FormContainer>
     </Container>
   )
--- src/Components/LoginTextInput/index.js
+++ src/Components/LoginTextInput/index.js
@@ -4,15 +4,24 @@ import styled from 'styled-components'
 const Container = styled.div`
 
 `
+const Input = styled.input`
+  border:1px solid #a6a6a6;
+  border-top-color: #949494;
+  border-radius:3px;
+  box-shadow:0 1px 0 rgba(255,255,255,0.5);
+  width:100%;
+  height:2.5em;
+  margin:10px auto;
+  margin-top:0px;
+`
 
 
-
-const LoginTextInput = ()=>{
+const LoginTextInput = ({label})=>{
 
   return(
     <Container>
-        <label htmlFor="login"/>
-        <input id="login" />
+        <label htmlFor="login">{label}</label>
+        <Input id="login" />
     </Container>
   )
 }
